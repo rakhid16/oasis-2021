5bebae714e94c221516a2a6a3227dd7109ff418e
Rakhid16 <roploverz@gmail.com>
2021-08-23 07:28:44 +0700

    main file

---
+++ main_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+from joblib import load
+
+model = load('models/model.pkl')
+
+form = st.form(key='my-form')
+
+clump_thickness = form.text_input('Masukkan nilai clump_thickness : ')
+uniformity_cell_size = form.text_input('Masukkan nilai uniformity_cell_size : ')
+uniformity_cell_shape = form.text_input('Masukkan nilai uniformity_cell_shape : ')
+marginal_adhesion = form.text_input('Masukkan nilai marginal_adhesion : ')
+single_epithelial_cell_size = form.text_input('Masukkan nilai single_epithelial_cell_size : ')
+bare_nuclei = form.text_input('Masukkan nilai bare_nuclei : ')
+bland_chromatin = form.text_input('Masukkan nilai bland_chromatin : ')
+normal_nucleoli = form.text_input('Masukkan nilai normal_nucleoli : ')
+mitoses = form.text_input('Masukkan nilai mitoses : ')
+
+submit = form.form_submit_button("Classify!")
+
+if submit:
+    st.write("Hasil perhitungan")
+    st.write("Untuk variabel Protein",
+        model.predict([[
+            float(clump_thickness),
+            float(uniformity_cell_size),
+            float(uniformity_cell_shape),
+            float(marginal_adhesion),
+            float(single_epithelial_cell_size),
+            float(bare_nuclei),
+            float(bland_chromatin),
+            float(normal_nucleoli),
+            float(mitoses)]])[0])
